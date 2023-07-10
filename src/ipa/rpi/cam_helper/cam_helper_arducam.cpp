2a4fb2eda053a5bb7dcb8bec0408a434bb5de789
tom-ard <tom@arducam.com>
2023-07-10 06:40:56 +0100

    ipa: raspberrrypi: Change Arducam-Pivariety CamHelper Name
    Change Arducam-Pivariety CamHelper Name to arducam-pivariety.
    Just using arducam as the name causes arducam_64mp to use the wrong
    CamHelper.

--- src/ipa/rpi/cam_helper/cam_helper_arducam.cpp
+++ src/ipa/rpi/cam_helper/cam_helper_arducam.cpp
@@ -134,4 +134,5 @@ static CamHelper *create()
 	return new CamHelperArducam();
 }
 
-static RegisterCamHelper reg("arducam", &create);
+static RegisterCamHelper reg("arducam-pivariety", &create);
+static RegisterCamHelper regAlias("arducam_pivariety", &create);
